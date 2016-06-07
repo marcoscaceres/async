0b56507490e7c75ffa672fe0b1ad34d428111449
Marcos Caceres <marcos@marcosc.com>
2016-06-07 18:01:01 +1000

    Feat(tests): Rewrote tests

--- test/async_spec.js
+++ test/async_spec.js
@@ -1,105 +1,160 @@
-/*globals fetch, async, expect */
-/*jshint mocha: true, noyield: true*/
+/*globals async*/
+/*jshint mocha: true, noyield: true, node: true*/
+"use strict";
 describe("async API", () => {
-  "use strict";
-  it("should reject when invoked with no arguments.", () => {
-    var test = async();
-    return test().should.be.rejected;
-  });
-
-  it("should reject when invoked with undefined.", () => {
-    var test = async(undefined);
-    return test().should.be.rejected;
-  });
-
-  it("should reject when invoked without a non-callable.", () => {
-    var test = async("String can't be called!");
-    return test().should.be.rejected;
-  });
-
-  it("should asynchronously resolve to the string pass.", () => {
-    var test = async(function*() {
-      var x = yield Promise.resolve("pass");
-      return x;
+  it("returns returns a function that returns promise.", () => {
+    const funct = async(function*(){
+      return "pass";
     });
-    return test().should.become("pass");
+    (typeof funct).should.equal("function");
+    const promise = funct();
+    promise.should.be.instanceof(Promise);
+    promise.should.become("pass");
   });
 
-  it("should reject when rejected.", () => {
-    var error = new Error("testing111");
-    var test = async(function*() {
+  it("rejects when called with non-callable.", () => {
+    async()().should.be.rejectedWith(TypeError);
+    async(null)().should.be.rejectedWith(TypeError);
+    async("a string")().should.be.rejectedWith(TypeError);
+    async({})().should.be.rejectedWith(TypeError);
+  });
+
+  it("immediately resolves when passed function.", () => {
+    const test = async(() => "pass");
+    test().should.eventually.become("pass");
+  });
+
+  it("accepts multiple arguments.", (done) => {
+    const test = async(function(arg1, arg2, arg3) {
+      let result = {arg1, arg2, arg3, length: arguments.length};
+      return result;
+    });
+    test(1, 2, 3)
+      .then(
+        result => result.should.deep.eql({arg1: 1, arg2: 2, arg3: 3, length: 3 })
+      )
+      .then(()=> done())
+      .catch(err => console.log(err.stack));
+  });
+
+  it("asynchronously resolves to the value of a resolved promise.", () => {
+    const test = async(function*() {
+      return yield Promise.resolve().then(() => "pass");
+    });
+    test().should.eventually.become("pass");
+  });
+
+  it("rejects when the generator throws.", () => {
+    const error = new Error("testing111");
+    const test = async(function*() {
       throw error;
     });
-    return test().should.eventually.be.rejectedWith(error);
+    test().should.be.rejectedWith(error);
   });
 
-  it("should recover and return in a catch.", () => {
-    var error = new Error("");
-    var test = async(function*() {
+  it("recovers and returns in a catch.", () => {
+    const error = new Error("");
+    const test = async(function*() {
       try {
         yield Promise.reject(error);
       } catch (err) {
         return "pass";
       }
+      return "fail";
     });
-    return test().should.become("pass");
+    test().should.become("pass");
   });
 
-  it("Should reject after throwing.", () => {
-    var error = new Error("Error");
-    var test = async(function*() {
+  it("rejects after throwing.", () => {
+    const error = new Error("Error");
+    const test = async(function*() {
       try {
         yield Promise.reject(error);
       } catch (err) {
         throw err;
       }
     });
-    return test().should.eventually.be.rejectedWith(error);
+    test().should.be.rejectedWith(error);
   });
 
-  it("should recover from rejection.", () => {
-    var test = async(function*() {
-      var result = "";
+  it("recovers from rejection.", () => {
+    const test = async(function*() {
       try {
         yield Promise.reject(new Error("pass"));
       } catch (error) {
-        result = error.message;
+        return error.message;
       }
-      return result;
     });
-    return test().should.become("pass");
+    test().should.become("pass");
   });
 
-  it("should recover from rejection and continue asynchronously.", () => {
-    var test = async(function*() {
-      var err = "";
+  it("recovers from rejection and continues asynchronously.", () => {
+    const test = async(function*() {
+      let err = "";
       try {
         yield Promise.reject(new Error("exception_"));
       } catch (ex) {
         err = ex.message;
       }
-      var w = yield (err + "recovered_");
-      var z = yield (w + "123");
+      const w = yield Promise.resolve("recovered_").then((v) => err + v);
+      const z = yield Promise.resolve("123").then((v) => w + v );
       return z; //exception_recovered_123
     });
-    return test().should.become("exception_recovered_123");
+    var p = test();
+    p.should.eventually.become("exception_recovered_123");
   });
 
-  it("should recover from rejection, then resolve with a string", () => {
-    var test = async(function*() {
+  it("recovers from rejection, then resolves with a string.", () => {
+    const test = async(function*() {
       try {
         yield Promise.reject(new Error());
-      } catch (err) {
-      }
-      var w = yield "recovered";
-      var y = yield w + `123`;
-      var z = yield Promise.resolve(y);
+      } catch (err) {}
+      const w = yield "recovered";
+      const y = yield w + `123`;
+      const z = yield Promise.resolve(y);
       return z;
     });
-    return test().should.become("recovered123");
+    test().should.become("recovered123");
   });
 
-  it("should fetch 10 pages and resolve.", () => {
+  it("accepts an argument.", () => {
+    const test = async(function*(arg1) {
+      return arg1;
+    });
+    test("pass").should.become("pass");
+  });
+
+  describe("binding tests", () => {
+    const obj = { value: "123", value2: 321 };
+
+    it("binds correctly when passed an object to bind to.", () => {
+      const test = async(function*(arg1) {
+        let result = {
+          value: this.value === "123",
+          value2: this.value2 === 321,
+          arg1: arg1 === undefined
+        };
+        return result;
+      }, obj);
+      test().should.eventually.deep.equal({ value: true, value2: true, arg1: true });
+    });
+
+    it("binds and accepts arguments.", () => {
+      const test = async(function*(arg1, arg2, arg3) {
+        let t1 = yield(this.value === "123");
+        let t2 = yield(this.value2 === 321);
+        let t3 = yield(arg1 === 1);
+        let t4 = yield(arg2 === 2);
+        let t5 = yield(arg3 === 3);
+        let t6 = yield(arguments.length === 3);
+        let r = yield(t1 && t2 && t3 && t4 && t5 && t6); //All should be true
+        return r;
+      }, obj);
+      test(1, 2, 3).should.become(true);
+    });
+  });
+
+  it("fetches 10 pages and resolves.", () => {
     var test = async(function*() {
       for (var i = 0; i < 10; i++) {
         var r = yield fetch("/?test=" + i);
@@ -110,118 +165,64 @@ describe("async API", () => {
     test().should.become("pass");
   });
 
-  it("should accept an argument being undefined.", () => {
-    var test = async(function*(arg1) {
-      return String(arg1);
-    });
-    return test().should.become("undefined");
-  });
-
-  it("should accept an argument.", () => {
-    var test = async(function*(arg1) {
-      return arg1;
-    });
-    return test("pass").should.become("pass");
-  });
-
-  it("should accept multiple arguments.", () => {
-    var test = async(function*(arg1, arg2, arg3) {
-      var result = {
-        arg1: arg1 === 1,
-        arg2: arg2 === 2,
-        arg3: arg3 === 3,
-        length: arguments.length === 3,
-      };
-      result = yield Promise.resolve(result);
-      return result;
-    });
-    test(1, 2, 3).should.eventually.deep.equal(
-      {arg1: true, arg2: true, arg3: true, length: 3}
-    );
-  });
-
-  it("should work even when async is not actually async.", () => {
-    var test = async(function() {
-      return "pass";
-    });
-    return test().should.become("pass");
-  });
-
-  describe("binding tests", ()=> {
-    var obj = {value: "123", value2: 321};
-
-    it("should bind correctly when passed an object to bind to.", () => {
-      var test = async(function*(arg1) {
-        var result = {
-          value: this.value === "123",
-          value2: this.value2 === 321,
-          arg1: arg1 === undefined
-        };
-        return result;
-      }, obj);
-      return test().should.eventually.deep.equal(
-        {value: true, value2: true, arg1: true}
-      );
-    });
-
-    it("should bind and accept arguments.", () => {
-      var test = async(function*(arg1, arg2, arg3) {
-        var t1 = yield (this.value === "123");
-        var t2 = yield (this.value2 === 321);
-        var t3 = yield (arg1 === 1);
-        var t4 = yield (arg2 === 2);
-        var t5 = yield (arg3 === 3);
-        var t6 = yield (arguments.length === 3);
-        var r = yield (t1 && t2 && t3 && t4 && t5 && t6); //All should be true
-        return r;
-      }, obj);
-      return test(1, 2, 3).should.become(true);
-    });
-  });
-
   describe("Thenable compatibility", () => {
-    it("Should resolve a thenable", ()=> {
-      var thenable = {
-        then() {
-          return "true";
-        }
-      };
-      var test = async(function*() {
-        var result = yield thenable;
-        expect(result).to.be(true);
+    it("resolves a thenable.", () => {
+      const test = async(function*() {
+        let thenable = {
+          then() {
+            return Promise.resolve("true");
+          }
+        };
+        let result = yield thenable;
+        return result;
       });
-      async.task(test);
+      test().should.eventually.become("true");
     });
   });
 
-  describe("async's task() method", ()=> {
-    it("should be a function", ()=> {
-      expect(async).to.have.property("task");
-      expect(async.task instanceof Function).to.equal(true);
+  describe("async's task() method", () => {
+    it("is a function.", () => {
+      async.should.have.property("task");
+      (typeof async.task).should.equal("function");
     });
-    it("should work with a generator", ()=> {
-      var test = function*() {
+    it("works with a generator.", () => {
+      const test = function*() {
         yield "test";
         return "pass";
       };
-      async.task(test).should.become("pass");
+      const p = async.task(test);
+      p.catch(err => {
+        debugger;
+        console.log(err)
+      })
+      p.should.become("pass");
     });
-    it("should work with a function", ()=> {
-      var test = function() {
+    it("works with a function.", () => {
+      const test = function() {
         return "pass";
       };
-      async.task(test).should.eventually.become("pass");
+      const p = async.task(test);
+      p.catch(err => {
+        debugger;
+        console.log(err)
+      })
+      p.should.eventually.become("pass");
     });
-    it("Should reject after throwing.", () => {
-      var error = new Error("Error");
-      var test = function*() {
+    it("rejects after throwing.", () => {
+      let error = new Error("Error");
+      function* test() {
+        debugger;
         try {
           yield Promise.reject(error);
         } catch (err) {
           throw err;
         }
       };
-      return async.task(test).should.eventually.be.rejectedWith(error);
+      const p = async.task(test);
+      p.catch(err => {
+        console.log(err)
+      })
+      p.should.eventually.be.rejectedWith(error);
     });
   });
 });
